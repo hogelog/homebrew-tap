27c836a00f8f81bfbd6fe4ec889a230317eb9aff
hogelog <konbu.komuro@gmail.com>
2025-07-03 11:05:05 +0900

    Update tcdd 0.0.2

--- tcdd.rb
+++ tcdd.rb
@@ -4,11 +4,12 @@
 class Tcdd < Formula
   desc "tcdd is useful cd command under tmux environment."
   homepage "https://github.com/hogelog/tcdd"
-  url "https://github.com/hogelog/tcdd/archive/refs/tags/0.0.1.tar.gz"
-  version "0.0.1"
-  sha256 "5586f20485d0c4e149152fa7c4cb4f60dda1de916b1ec6847cd54b9740cb7962"
+  url "https://github.com/hogelog/tcdd/archive/refs/tags/0.0.2.tar.gz"
+  version "0.0.2"
+  sha256 "a060514047f4f7ff870a1ee9e61bfecb69dcc8ca"
   license "MIT"
 
+  depends_on "bash"
   depends_on "tmux"
 
   def install
